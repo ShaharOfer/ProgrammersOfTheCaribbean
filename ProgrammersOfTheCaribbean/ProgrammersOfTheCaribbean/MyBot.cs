9d558138961ec6fa7ad888c5015234917dadd669
shaharofer283 <shaharofer28@gmail.com>
2020-07-19 10:33:56 +0300

    First bot

--- ProgrammersOfTheCaribbean/ProgrammersOfTheCaribbean/MyBot.cs
+++ ProgrammersOfTheCaribbean/ProgrammersOfTheCaribbean/MyBot.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Pirates;
 
 namespace ProgrammersOfTheCaribbean
@@ -7,7 +8,42 @@ namespace ProgrammersOfTheCaribbean
     {
         public void DoTurn(IPirateGame state)
         {
-            throw new NotImplementedException();
+            // Initing stuff
+            List<Island> islands = state.NeutralIslands();
+            List<Pirate> myPirates = state.AllMyPirates();
+            Dictionary<Pirate, Island> pirateToIsland = new Dictionary<Pirate, Island>();
+
+
+            for (int i = 0; i < myPirates.Count; i++)
+            {
+                if (i < islands.Count)
+                {
+                    pirateToIsland.Add(myPirates[i], islands[i]);
+                }
+            }
+
+            List<Island> lostIslands = state.EnemyIslands();
+            for (int j = islands.Count; j < myPirates.Count; j++) 
+            {
+                pirateToIsland.Add(myPirates[j], lostIslands[j - islands.Count]);
+            }
+
+            myPirates.ForEach(pirate =>
+            {
+                Island island = pirateToIsland[pirate];
+                List<Direction> movingDirections = state.GetDirections(pirate, island);
+                state.Debug($"Pirate: {pirate.Id}\nLocation: {pirate.Loc.Row}, {pirate.Loc.Col}\nMove to: ");
+                foreach(var direction in movingDirections)
+                {
+                    state.Debug(direction.ToString());
+                }
+
+                if (movingDirections.Count > 0)
+                {
+                    state.SetSail(pirate, movingDirections[0]);
+                }
+
+            });
         }
     }
 }
