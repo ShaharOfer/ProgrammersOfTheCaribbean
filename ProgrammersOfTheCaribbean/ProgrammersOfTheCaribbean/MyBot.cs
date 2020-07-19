bd6b7be4a25f09e8f7e4691f936ded47e5faf692
shaharofer283 <shaharofer28@gmail.com>
2020-07-19 09:48:29 +0300

    Add class

---
+++ ProgrammersOfTheCaribbean/ProgrammersOfTheCaribbean/MyBot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pirates;
+
+namespace ProgrammersOfTheCaribbean
+{
+    class MyBot : IPirateBot
+    {
+        public void DoTurn(IPirateGame state)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
