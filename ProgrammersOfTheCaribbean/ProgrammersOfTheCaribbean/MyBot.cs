0cfef343ed4835ef2dcd6b62625d735290b2f5ff
shaharofer283 <shaharofer28@gmail.com>
2020-07-19 18:05:50 +0300

    Add skeleton

--- ProgrammersOfTheCaribbean/ProgrammersOfTheCaribbean/MyBot.cs
+++ ProgrammersOfTheCaribbean/ProgrammersOfTheCaribbean/MyBot.cs
@@ -1,103 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Pirates;
 
 namespace ProgrammersOfTheCaribbean
 {
-    class MyBot : IPirateBot
+    public class MyBot : IPirateBot
     {
+        private Dictionary<Pirate, Island> _pirateToIsland = new Dictionary<Pirate, Island>();
+        
         public void DoTurn(IPirateGame state)
         {
-            // Initing stuff
-            List<Island> islands = state.NotMyIslands();
-            List<Pirate> myPirates = state.MyPirates();
-            List<Pirate> copyPirates = new List<Pirate>(myPirates);
-            Dictionary<Pirate, Island> pirateToIsland = new Dictionary<Pirate, Island>();
-
-            for (int i = 0; i < islands.Count; i++)
+            List<Pirate> myPirates = state.AllMyPirates();
+            myPirates.Reverse();
+            if (_pirateToIsland.Count == 0)
             {
-                Pirate closestPirate = GetClosestPirate(state, islands[i], myPirates);
-
-                //state.Debug($"closest: {closestPirate.Id.ToString()} to island: {islands[i]}");
-                if (closestPirate != null)
-                {
-                    myPirates.Remove(closestPirate);
-                    pirateToIsland.Add(closestPirate, islands[i]);
-                }
+                // Need to allocate pirates to islands
+                AllocatePiratesToIslands(state, myPirates, state.Islands());
             }
+            // Here the pirates know their target islands
 
-            copyPirates.ForEach(pirate =>
-            {
-                if (pirateToIsland.ContainsKey(pirate) && pirateToIsland[pirate] != null)
-                {
-                    Island island = pirateToIsland[pirate];
-                    state.Debug($"Pirate {pirate.Id.ToString()} to island {island.Id.ToString()}");
-                    List<Direction> movingDirections = state.GetDirections(pirate, island);
-
-                    if (movingDirections.Count > 0 && !pirate.IsLost)
-                    {
-                        state.Debug($"Pirate: {pirate.Id.ToString()}");
-                        movingDirections.ForEach(direc =>
-                        {
-                            state.Debug($"{direc}");
-                        });
-                        state.SetSail(pirate, movingDirections[0]);
-                    }
-                }
-                else // Bot that has no island
-                {
-                    state.Debug($"Nothing {pirate.Id.ToString()}");
-                    Island closestIsland = GetClosestIsland(state, pirate, state.NotMyIslands());
-                    if (closestIsland != null)
-                    {
-                        List<Direction> movingDirections = state.GetDirections(pirate, closestIsland);
-                        state.SetSail(pirate, movingDirections[0]);
-                    }
-                }
-
-            });
-        }
-
-        private Island GetClosestIsland(IPirateGame state, Pirate myPirate, List<Island> islands)
-        {
-            // Move from the island
-            // Move to closest attacked island
-            // 
-            int closetDIstance = int.MaxValue;
-            Island closestIsland = null;
-            islands.ForEach(island =>
-            {
-                int distnace = state.Distance(myPirate, island);
-                if (distnace < closetDIstance) {
-                    closestIsland = island;
-                    closetDIstance = distnace;
-                }
-            });
-
-            return closestIsland;
-        }
-
-        private Pirate GetClosestPirate(IPirateGame state, Island island, List<Pirate> pirates)
-        {
-            // Move from the island
-            // Move to closest attacked island
-            // 
-            int closetDIstance = int.MaxValue;
-            Pirate closestPirate = null;
-            pirates.ForEach(pirate =>
+            myPirates.ForEach(pirate =>
             {
                 if (!pirate.IsLost)
                 {
-                    int distnace = state.Distance(pirate, island);
-                    if (distnace < closetDIstance)
-                    {
-                        closestPirate = pirate;
-                        closetDIstance = distnace;
-                    }
+                    var directions = state.GetDirections(pirate, _pirateToIsland[pirate]);
+                    state.SetSail(pirate, directions[0]);
                 }
             });
+         
+        }
 
-            return closestPirate;
+        private void AllocatePiratesToIslands(IPirateGame state, List<Pirate> myPirates, List<Island> islands)
+        {
+            int piratesIndex = 0;
+            int currentIslandIndex = 0;
+            var topClosestIslands = GetThreeClosestIslands(state, islands, myPirates[0]);
+
+            int numberOfPirets = (int)Math.Floor(myPirates.Count * 0.2);
+            AllocatePiretsToIsland(topClosestIslands[currentIslandIndex], myPirates, piratesIndex, piratesIndex + numberOfPirets); ;
+            currentIslandIndex++;
+            piratesIndex += numberOfPirets;
+
+            numberOfPirets = (int)Math.Floor(myPirates.Count * 0.6);
+            AllocatePiretsToIsland(topClosestIslands[currentIslandIndex], myPirates, piratesIndex, piratesIndex + numberOfPirets); ;
+            currentIslandIndex++;
+            piratesIndex += numberOfPirets;
+
+            numberOfPirets = (int)Math.Floor(myPirates.Count * 0.2);
+            AllocatePiretsToIsland(topClosestIslands[currentIslandIndex], myPirates, piratesIndex, piratesIndex + numberOfPirets); ;
+        }
+
+        private List<Island> GetThreeClosestIslands(IPirateGame state, List<Island> islands, Pirate pirate)
+        {
+            var topClosestIslands = islands.OrderBy(island => state.Distance(pirate, island)).Take(3).ToList();
+            return topClosestIslands;
+        }
+
+        private void AllocatePiretsToIsland(Island islandToBeAllocated, List<Pirate> pirets, int startPiretIndex, int endPiretIndex)
+        {
+            for (int i = startPiretIndex; i < endPiretIndex; i++)
+            {
+                _pirateToIsland.Add(pirets[i], islandToBeAllocated);
+            }
         }
     }
 }
